5dac1a65c4446cd242c580d82cddc04e1b4ac52e
Dawn Mills <dawnwmills@aol.com>
2017-08-31 22:11:32 -0400

    uploading files for assignment

---
+++ BasicCard.js
@@ -0,0 +1,6 @@
+/* ==This file should define a Node module that exports a constructor for creating basic flashcard, e.g. 'module.exports=BasicCard;'
+===== The constructor should accept two arguments: 'front' and 'back'.
+===== The constructed objects should have a 'front' property that contains the text on the front of the card.
+===== The constructed objects should have a 'back' property that contains the text on the back of the card.
+===== ================================================*/
+
